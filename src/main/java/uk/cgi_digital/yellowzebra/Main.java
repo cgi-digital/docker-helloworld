28afcd3da580251d9c1bcd77e916d5d99306e5e7
AndrewG <tech@andrewgorton.uk>
2017-11-30 16:08:35 +0000

    Added some garbage code.

--- src/main/java/uk/cgi_digital/yellowzebra/Main.java
+++ src/main/java/uk/cgi_digital/yellowzebra/Main.java
@@ -3,5 +3,8 @@ package uk.cgi_digital.yellowzebra;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int c;
+        c = 10;
     }
 }
