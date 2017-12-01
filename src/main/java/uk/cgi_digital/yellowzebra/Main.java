e185d3f5cde011080a487d162d94fcdd7ed855b3
AndrewG <tech@andrewgorton.uk>
2017-12-01 13:21:21 +0000

    Breaking for SonarQube failures.

--- src/main/java/uk/cgi_digital/yellowzebra/Main.java
+++ src/main/java/uk/cgi_digital/yellowzebra/Main.java
@@ -3,5 +3,12 @@ package uk.cgi_digital.yellowzebra;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int c = Integer.MAX_VALUE;
+        c++;
+
+        if (true) {
+            System.out.println("Something here");
+        }
     }
 }
