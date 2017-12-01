c423a495f88eb9763c7cd0b4cb616a10156cf0db
AndrewG <tech@andrewgorton.uk>
2017-12-01 12:31:32 +0000

    Added unmaintainable code.

--- src/main/java/uk/cgidigital/yellowzebra/Main.java
+++ src/main/java/uk/cgidigital/yellowzebra/Main.java
@@ -2,6 +2,10 @@ package uk.cgidigital.yellowzebra;
 
 public class Main {
     public static void main(String[] args) {
+
         System.err.println("Hello world!");
+
+        int c;
+        c = 10;
     }
 }
