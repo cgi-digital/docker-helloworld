9f5bb75eb1295633931629e2d972e8821286c4a7
AndrewG <tech@andrewgorton.uk>
2017-12-01 12:41:50 +0000

    SonarQube attempts to fail.

--- src/main/java/uk/cgi_digital/yellowzebra/Main.java
+++ src/main/java/uk/cgi_digital/yellowzebra/Main.java
@@ -2,11 +2,17 @@ package uk.cgi_digital.yellowzebra;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
 
         int c;
         c = 10;
         int d = 20;
+
+
+        if (true) {
+            System.out.println("I am alive " + d);
+        }
+
+        System.err.println("Completed.");
     }
 }
